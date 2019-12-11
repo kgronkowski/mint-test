910f2b1fe11e2f250481962dfb0f5108f5a5852c
Krzystof Gronkowski <gronkowski@8powers.pl>
2019-12-11 22:13:22 +0100

    add main controller

---
+++ src/Controller/AppController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+
+}
